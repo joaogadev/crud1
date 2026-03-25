8ea61340d26affa258ffccb36b8a9a82e9ccac15
João Gabriel <joaogabriel.trindade04@gmail.com>
2026-03-24 22:20:17 -0300

    fix: corrigi tabela,JPA não estava puxando a tabela correta

--- src/main/java/com/api/crup1/Model/UserModel.java
+++ src/main/java/com/api/crup1/Model/UserModel.java
@@ -1,11 +1,9 @@
 package com.api.crup1.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
